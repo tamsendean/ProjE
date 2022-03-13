e59e04fa9b672ee93134ddb5725dbc2c55f005da
tamsendean <deant23@up.edu>
2022-03-13 16:19:02 -0700

    part1

--- app/src/main/java/edu/up/dbgui/MainActivity.java
+++ app/src/main/java/edu/up/dbgui/MainActivity.java
@@ -3,6 +3,10 @@ package edu.up.dbgui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button testButton = findViewById(R.id.testButton);
+        EditText text = findViewById(R.id.editText);
+
+        testButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(" ");
+                //A new instance of the game state class is created using the default constructor and
+                //assigned to a variable named firstInstance
+                // create a deep copy of firstInstance from the perspective of player one, assign to firstCopy.
+                //
+            }
+        });
     }
 }
